93a4293df2138760893aea553492113d65442e90
djkah11 <djkah11@yandex.ru>
2016-10-28 22:31:16 +0300

    update karma config

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
+'use strict';
+
 module.exports = function (config) {
-	'use strict';
-	config.set({
+
+	var configuration = {
 
 		basePath: '',
 
@@ -12,7 +14,7 @@ module.exports = function (config) {
 
 		reporters: ['progress', 'coverage'],
 		preprocessors: {
-			'./public/build/*.js': ['coverage'],
+			'./public/build/*.js':['coverage']
 		},
 
 		port: 9876,
@@ -23,11 +25,27 @@ module.exports = function (config) {
 		// level of logging
 		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
 		logLevel: config.LOG_INFO,
-
+		plugins: [
+			'karma-jasmine',
+			'karma-chrome-launcher',
+			'karma-coverage'
+		],
 		browsers: ['Chrome'],
+		customLaunchers: {
+			Chrome_travis_ci: {
+				base: 'Chrome',
+				flags: ['--no-sandbox']
+			}
+		},
 		coverageReporter: {
 			type: 'html',
 			dir: 'public/coverage/'
 		}
-	});
+	};
+
+	if (process.env.TRAVIS) {
+		configuration.browsers = ['Chrome_travis_ci']
+	}
+
+	config.set(configuration)
 };
