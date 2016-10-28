4bfa05d6ec292487a4069fc87f3518300a27b761
djkah11 <djkah11@yandex.ru>
2016-10-28 21:41:50 +0300

    remove unused info from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,10 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: {
     'main': ["./public/main.js"],
-    'login': ["./public/views/login.js"],
-    'register': ["./public/views/register.js"],
-    'singlePlayer': ["./public/views/singlePlayer.js"],
-    'scoreboard': ["./public/views/scoreboard.js"],
-    'route.spec': ["./public/tests/route.spec.js"],
   },
   output: {
     path: "./public/build",
-    filename: "[name].js"
+    filename: "superfile.js"
   },
   watch: true,
   devtool: "source-map",
