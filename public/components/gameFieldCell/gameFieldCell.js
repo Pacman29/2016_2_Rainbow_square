a7f83a160612962cd9c0f38863da8b0d2834b039
Ermishechkin Alexandr <erman15@yandex.ru>
2016-12-06 01:31:37 +0300

    Add functionality and styles to gameFieldCell

--- public/components/gameFieldCell/gameFieldCell.js
+++ public/components/gameFieldCell/gameFieldCell.js
@@ -4,5 +4,20 @@ export default class GameFieldCell extends Block {
   constructor(options = {}) {
     super('div', options);
     this._el.classList.add('gameFieldCell');
+    this.state = 0;
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  set state(value) {
+    function stateName(state) {
+      return "gameFieldCell_" + state;
+    }
+    var el = this._el;
+    el.classList.remove(stateName(this._state));
+    this._state = value;
+    el.classList.add(stateName(this._state));
   }
 }
